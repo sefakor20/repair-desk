353e116931d1c7a8985d87e74d3cb0e5b1f2d468
sefakor20 <sefakorhom2012@gmail.com>
2025-10-05 23:16:00 +0000

    Enhance AppServiceProvider with eager loading and password rule configurations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Automatically eager load relationships
+        Model::automaticallyEagerLoadRelationships();
+
+        // In production, merely log lazy loading violations.
+        if ($this->app->isProduction()) {
+            Model::handleLazyLoadingViolationUsing(function ($model, $relation): void {
+                $class = get_class($model);
+
+                info("Attempted to lazy load [{$relation}] on model [{$class}].");
+            });
+        }
+
+        // Set default password rule
+        Password::defaults(function () {
+            $rule = Password::min(8);
+
+            return $this->app
+                ->isProduction()
+                ? $rule->mixedCase()->letters()->numbers()->symbols()->uncompromised()
+                : $rule;
+        });
     }
 }
